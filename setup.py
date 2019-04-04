448d30829061ab32347e8fc6b091cc772ac5f484
gui11aume <guillaume.filion@gmail.com>
2019-04-04 11:23:23 +0200

    First commit.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name="gzopen",
+   version = '1.0',
+   description = 'gzopen module',
+   author_email = 'guillaume.filion@gmail.com',
+   long_description = 'gzopen module',
+   packages=["."],
+)
